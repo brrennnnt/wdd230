df380667cb3b8d483ac10c628c20bf60839847d4
brrennnnt <rot22001@byui.edu>
2023-09-16 16:18:20 -0700

    getDates.js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,12 @@
 /*current year alert and timestamp */
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the copyright year and set it dynamically
+    const copyrightYearElement = document.getElementById("copyrightYear");
+    const currentYear = new Date().getFullYear();
+    copyrightYearElement.textContent = currentYear;
+
+    // Get the last modified date and set it dynamically
+    const lastModifiedElement = document.getElementById("lastModified");
+    const lastModifiedDate = new Date(document.lastModified);
+    lastModifiedElement.textContent = lastModifiedDate.toLocaleString();
+});
