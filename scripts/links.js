b890c9a6c67caf89292877098a64351b9d20fc2d
brrennnnt <rot22001@byui.edu>
2023-10-12 20:48:51 -0700

    test

---
+++ scripts/links.js
@@ -0,0 +1,38 @@
+const baseURL = "https://brrennnnt.github.io/wdd230/"; // Your base URL
+const linksURL = "https://brrennnnt.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+getLinks();
+
+const displayLinks = (weeks) => {
+    const cardsElement = document.querySelector('.cards');
+
+    weeks.forEach((weekItem) => {
+        const weekDiv = document.createElement('div');
+        weekDiv.classList.add('week'); // You can add a CSS class to style this div if needed.
+
+        const weekHeading = document.createElement('h2');
+        weekHeading.textContent = weekItem.week;
+        weekDiv.appendChild(weekHeading);
+
+        const linksList = document.createElement('ul');
+
+        weekItem.links.forEach((link) => {
+            const listItem = document.createElement('li');
+            const linkAnchor = document.createElement('a');
+            linkAnchor.textContent = link.title;
+            linkAnchor.href = baseURL + link.url; // Create the full URL.
+            linkAnchor.target = "_blank"; // Opens links in a new tab.
+            listItem.appendChild(linkAnchor);
+            linksList.appendChild(listItem);
+        });
+
+        weekDiv.appendChild(linksList);
+        cardsElement.appendChild(weekDiv);
+    });
+}
---
+++ scripts/weather.js
@@ -0,0 +1,33 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const units = 'imperial';
+
+const apiKey = 'a9fc20b877b479b87781d3e44c1ca34e'; // Replace with your actual API key
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=32.84&lon=-109.71&units=${units}&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
